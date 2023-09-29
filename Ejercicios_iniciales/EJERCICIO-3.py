3a2404a6e4f940322c2c7f180a60b52811ba7186
huguss2008 <hugo.perdiguero@campus.xaloc.org>
2023-09-29 12:15:32 +0200

    Create EJERCICIO-3.py

---
+++ Ejercicios_iniciales/EJERCICIO-3.py
@@ -0,0 +1,9 @@
+# Solicitar al usuario ingresar dos números enteros
+var_1 = int(input("Ingresa el primer número entero: "))
+var_2 = int(input("Ingresa el segundo número entero: "))
+
+# Realiza la suma de los dos números
+suma = var_1 + var_2
+
+# Muestra el resultado
+print(f"La suma de {var_1} y {var_2} es igual a {suma}")
