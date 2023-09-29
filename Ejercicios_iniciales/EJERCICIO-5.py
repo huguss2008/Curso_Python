b9ee3fe210de282bc06639f5221189f1e85ff13c
huguss2008 <hugo.perdiguero@campus.xaloc.org>
2023-09-29 12:15:37 +0200

    Create EJERCICIO-5.py

---
+++ Ejercicios_iniciales/EJERCICIO-5.py
@@ -0,0 +1,13 @@
+# Solicita al usuario ingresar cinco palabras
+palabra_1 = input("Ingresa la primera palabra: ")
+palabra_2 = input("Ingresa la segunda palabra: ")
+palabra_3 = input("Ingresa la tercera palabra: ")
+palabra_4 = input("Ingresa la cuarta palabra: ")
+palabra_5 = input("Ingresa la quinta palabra: ")
+
+# Crea la frase con las palabras separadas por comas
+frase = f"{palabra_1}, {palabra_2}, {palabra_3}, {palabra_4}, {palabra_5}"
+
+# Muestra la frase
+print("La frase formada con las palabras es:")
+print(frase)
