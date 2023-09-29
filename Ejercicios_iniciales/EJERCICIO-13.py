8bb085e981c07eee59f0ed993b4c342c6ba3c4a2
huguss2008 <hugo.perdiguero@campus.xaloc.org>
2023-09-29 12:15:28 +0200

    Create EJERCICIO-13.py

---
+++ Ejercicios_iniciales/EJERCICIO-13.py
@@ -0,0 +1,12 @@
+# Solicitar al usuario la longitud del lado del cubo
+lado = float(input("Ingrese la longitud del lado del cubo: "))
+
+# Calcular el área del cubo (área de la superficie)
+area = 6 * (lado ** 2)
+
+# Calcular el volumen del cubo
+volumen = lado ** 3
+
+# Mostrar el área y el volumen por pantalla
+print(f"El área del cubo es: {area}")
+print(f"El volumen del cubo es: {volumen}")
