c14762ae33965151b11f61e39c83e89ca1a4050f
huguss2008 <hugo.perdiguero@campus.xaloc.org>
2023-09-29 12:15:34 +0200

    Create EJERCICIO-4.py

---
+++ Ejercicios_iniciales/EJERCICIO-4.py
@@ -0,0 +1,20 @@
+# Solicitar al usuario ingresar dos números enteros
+var_1 = int(input("Ingresa el primer número entero: "))
+var_2 = int(input("Ingresa el segundo número entero: "))
+
+# Realiza la suma de los dos números
+suma = var_1 + var_2
+
+# Muestra el resultado
+print(f"La suma de {var_1} y {var_2} es igual a {suma}")
+
+# Solicitar al usuario ingresar dos números decimales
+
+numdeci_1=float(input("Ingresa el primer número decimal: "))
+numdeci_2=float(input("Ingresa el segundo número decimal: "))
+
+#Realizar la suma de los dos números
+suma= numdeci_1 + numdeci_2
+
+#Mostrar el resultado
+print(f"La suma de {numdeci_1} y {numdeci_2} es igual a {suma}")
