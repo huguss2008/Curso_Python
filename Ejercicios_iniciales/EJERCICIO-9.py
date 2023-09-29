329a11e16befe5a6f5c341cc6cd2ba59f47826be
huguss2008 <hugo.perdiguero@campus.xaloc.org>
2023-09-29 12:15:46 +0200

    Create EJERCICIO-9.py

---
+++ Ejercicios_iniciales/EJERCICIO-9.py
@@ -0,0 +1,7 @@
+# Solicitar al usuario un número
+var_1=int(input("Introduce un número de horas: "))
+# Ejecutar la operación
+totalhoras=var_1/60
+totalsegundos=var_1/3600
+# Decir el resultado
+print(f"El número de minutos {totalhoras} y en segundos seria {totalsegundos}")
