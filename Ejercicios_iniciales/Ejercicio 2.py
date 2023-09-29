96208fe2a211897029e85c7db59e7d5968d82478
huguss2008 <hugo.perdiguero@campus.xaloc.org>
2023-09-29 12:15:13 +0200

    Create Ejercicio 2.py

---
+++ Ejercicios_iniciales/Ejercicio 2.py
@@ -0,0 +1,8 @@
+# Solicitar al usuario que introduzca un número entero, una letra y un número decimal
+númeroentero=int(input("Introduce un número entero: "))
+letra= input("Introduce una letra: ")
+númerodecimal=float(input("Introduce un número decimal: "))
+
+print("El número introducido: ",númeroentero)
+print("Tu letra es: ",letra)
+print("Tu numero decimal es: ",númerodecimal)
